f9a513d12899ecd511012958094129399c41d4b8
ValiyevAli <mcpegaming11@gmail.com>
2020-12-24 21:03:45 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+APP_ID = int(input("APP ID yazin: "))
+API_HASH = input("API HASH yazin: ")
+with TelegramClient(
+    StringSession(), 
+    APP_ID, 
+    API_HASH
+) as client:
+    session_str = client.session.save()
+    s_m = client.send_message("me", session_str)
+    print('Herokuya kecerek deploya davam edin.')
